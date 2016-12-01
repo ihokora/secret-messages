db91b25c6dcc7c689e562b5944bbe6d54ffcfb87
Ihor Sikora <sikoraigo@gmail.com>
2016-12-01 21:23:24 +0200

    Update create_message migration

--- db/migrations/20161128181609_create_messages.rb
+++ db/migrations/20161128181609_create_messages.rb
@@ -4,12 +4,13 @@ Hanami::Model.migration do
       primary_key :id
 
       column :encrypted_text,   String, null: false
-      column :token,            String, null: false
-
-      column :visits_remaining  Integer
+      column :key,              String, null: false
+      column :iv,               String
+      column :visits_remaining, Integer
       column :expiration_time,  DateTime
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
+    end
   end
 end
