78424ace0ea35409351df324e9d632be25118370
Ihor Sikora <sikoraigo@gmail.com>
2016-12-03 21:02:59 +0200

    Create simple message entity spec

--- spec/secret_messages/entities/message_spec.rb
+++ spec/secret_messages/entities/message_spec.rb
@@ -1,3 +1,13 @@
 RSpec.describe Message do
-  # place your tests here
+
+  it 'can be initialesed with attributes' do
+    message = Message.new(
+                          encrypted_text: 'I am encrypted text',
+                          visits_remaining: 2,
+                          expiration_time: 1 )
+    expect(message.encrypted_text).to eq('I am encrypted text')
+    expect(message.visits_remaining).to eq(2)
+    expect(message.expiration_time).to be_an_instance_of(Time)
+  end
+
 end
