a7a0a584ad9e75b19c595afc699b4d9a41afc6f7
Ihor Sikora <sikoraigo@gmail.com>
2016-12-08 23:36:58 +0200

    Basic implementationof 'show' action

--- apps/web/controllers/messages/show.rb
+++ apps/web/controllers/messages/show.rb
@@ -2,7 +2,10 @@ module Web::Controllers::Messages
   class Show
     include Web::Action
 
+    expose :message
+
     def call(params)
+      @message = MessageRepository.new.find(params[:id])
     end
   end
 end
--- apps/web/views/messages/show.rb
+++ apps/web/views/messages/show.rb
@@ -1,5 +1,17 @@
 module Web::Views::Messages
   class Show
     include Web::View
+
+    def message_decrypted_text
+      message.decrypt
+    end
+
+    def message_expiration_time
+      message.expiration_time
+    end
+
+    def message_visits_remains
+      message.visits_remains
+    end
   end
 end
