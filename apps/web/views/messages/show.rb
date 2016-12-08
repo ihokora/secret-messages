16858061a98c7aac3772c606c9bc83aae208ab01
Ihor Sikora <sikoraigo@gmail.com>
2016-12-09 01:44:21 +0200

    Add method for optional message info output

--- apps/web/views/messages/show.rb
+++ apps/web/views/messages/show.rb
@@ -6,12 +6,23 @@ module Web::Views::Messages
       message.decrypt
     end
 
+    def message_destruction_info
+      html.div class: 'message-info' do
+        message_expiration_time || message_visits_remains
+      end
+
+    end
+
     def message_expiration_time
-      message.expiration_time
+      if message.expiration_time
+        p "Message will be destroyed at: #{message.expiration_time}"
+      end
     end
 
     def message_visits_remains
-      message.visits_remains
+      if message.visits_remains
+        p "Vies left: #{message.visits_remains}"
+      end
     end
   end
 end
