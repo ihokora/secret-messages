3da4b05caac85dd7d5521257e5eb34e3af0c0d98
Ihor Sikora <sikoraigo@gmail.com>
2016-12-10 16:16:43 +0200

    Add accordion options, polish views

--- apps/web/views/messages/new.rb
+++ apps/web/views/messages/new.rb
@@ -1,5 +1,51 @@
 module Web::Views::Messages
   class New
     include Web::View
+
+    def accordion_options
+
+      html.div(class: 'panel-group', id: 'accordion') do
+        div(class: 'panel panel-default') do
+
+          div(class: 'panel-heading') do
+            h4 class: 'panel-title'
+              a 'Hours', 'data-toggle': 'collapse',
+                'data-parent': '#accordion', 'href': '#collapse1'
+          end
+
+          div(id: 'collapse1', class: 'panel-collapse collapse') do
+            div class: 'panel-body' do
+              div class: 'input-group' do
+                span 'Hours', class: 'input-group-addon'
+                input name: 'message[time_remains]', type: 'number', id: 'time_input', class: 'form-control',
+                      placeholder: 'Please enter humber of hours here', min: 1
+              end
+            end
+          end
+
+        end
+
+        div(class: 'panel panel-default') do
+
+          div(class: 'panel-heading') do
+            h4 class: 'panel-title'
+             a 'Views', 'data-toggle': 'collapse',
+                'data-parent': '#accordion', 'href': '#collapse2'
+          end
+
+          div(id: 'collapse2', class: 'panel-collapse collapse') do
+            div( class: 'panel-body') do
+              div class: 'input-group' do
+                span 'Views', class: 'input-group-addon'
+                input name: 'message[visits_remains]', type: 'number', id: 'visit_input', class: 'form-control',
+                      placeholder: 'Please enter humber of views here', min: 1
+              end
+            end
+          end
+
+        end
+
+      end
+    end
   end
 end
