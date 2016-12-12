baddc58444c30b10097edc4336a88ac981b1cd0a
Ihor Sikora <sikoraigo@gmail.com>
2016-12-12 03:04:28 +0200

    Fix bug when can submit wiht empty hours and visits params

--- apps/web/assets/javascripts/app.js
+++ apps/web/assets/javascripts/app.js
@@ -1,19 +1,17 @@
 $(function () {
 
   $('#collapse1').on('shown.bs.collapse', function () {
-    console.log("Change time to views");
-    document.getElementById('visit_input').name = "";
-    document.getElementById('visit_input').required = false;
-    document.getElementById('time_input').name = "message[time_remains]";
-    document.getElementById('time_input').required = true;
+    document.getElementById('visit-input').name = "";
+    document.getElementById('visit-input').required = false;
+    document.getElementById('time-input').name = "message[time_remains]";
+    document.getElementById('time-input').required = true;
   })
 
-
   $('#collapse2').on('shown.bs.collapse', function () {
-    document.getElementById('time_input').name = "";
-    document.getElementById('time_input').required = false;
-    document.getElementById('visit_input').name = "message[visits_remains]";
-    document.getElementById('visit_input').required = true;
+    document.getElementById('time-input').name = "";
+    document.getElementById('time-input').required = false;
+    document.getElementById('visit-input').name = "message[visits_remains]";
+    document.getElementById('visit-input').required = true;
   })
 
   $('#collapse1').on('hidden.bs.collapse', function () {
@@ -33,4 +31,4 @@ $(function () {
 });
 
 
-// document.getElementsByName('message[visits_remains]').value = 5;
+
--- apps/web/views/messages/new.rb
+++ apps/web/views/messages/new.rb
@@ -5,19 +5,17 @@ module Web::Views::Messages
     def accordion_options
 
       html.div(class: 'panel-group', id: 'accordion') do
-        div(class: 'panel panel-default') do
+        div(class: 'panel') do
 
-          div(class: 'panel-heading') do
-            h4 class: 'panel-title'
-              a 'Destroy by Hours', 'data-toggle': 'collapse',
-                'data-parent': '#accordion', 'href': '#collapse1', id: 'time_panel'
-          end
+          button 'Set Hours', class: 'btn btn-primary btn-block btn-sm', 'data-toggle': 'collapse',
+                 'data-parent': '#accordion', 'href': '#collapse1', id: 'time-button'
+
 
           div(id: 'collapse1', class: 'panel-collapse collapse') do
             div class: 'panel-body' do
               div class: 'input-group' do
                 span 'Hours', class: 'input-group-addon'
-                input name: 'message[time_remains]', type: 'number', id: 'time_input', class: 'form-control',
+                input name: '', type: 'number', id: 'time-input', class: 'form-control',
                       placeholder: 'Please enter humber of hours here', min: 1
               end
             end
@@ -25,20 +23,17 @@ module Web::Views::Messages
 
         end
 
-        div(class: 'panel panel-default') do
+        div(class: 'panel') do
 
-          div(class: 'panel-heading') do
-            h4 class: 'panel-title'
-             a 'Destroy by Views', 'data-toggle': 'collapse',
-                'data-parent': '#accordion', 'href': '#collapse2', id: 'visits_panel'
-          end
+          button 'Set Views', class: 'btn btn-primary btn-block btn-sm', 'data-toggle': 'collapse',
+                  'data-parent': '#accordion', 'href': '#collapse2', id: 'visits-button'
 
           div(id: 'collapse2', class: 'panel-collapse collapse in') do
             div( class: 'panel-body') do
               div class: 'input-group' do
                 span 'Views', class: 'input-group-addon'
-                input name: 'message[visits_remains]', type: 'number', id: 'visit_input', class: 'form-control',
-                      placeholder: 'Please enter humber of views here', min: 1
+                input name: 'message[visits_remains]', type: 'number', id: 'visit-input', class: 'form-control',
+                      placeholder: 'Please enter humber of views here', min: 1, required: true
               end
             end
           end
